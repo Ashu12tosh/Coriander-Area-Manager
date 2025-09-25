f29bc211ff3205d1f3ec916571a996723483e8de
ashu1212 <ashutosh@vijatshisoftware.com>
2025-09-25 12:37:59 +0530

    Add Submanagers option to ActionItems component

--- src/components/profile/ActionItems.js
+++ src/components/profile/ActionItems.js
@@ -3,7 +3,7 @@ import React from "react";
 import { View, TouchableOpacity, Text } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
-const ActionItems = ({ onNotificationPress  }) => {
+const ActionItems = ({ onNotificationPress, onSubmangerPress }) => {
   return (
     <View className="bg-white rounded-xl shadow-sm mb-4">
       <TouchableOpacity
@@ -11,6 +11,14 @@ const ActionItems = ({ onNotificationPress  }) => {
         className="flex-row justify-between items-center px-4 py-4 border-b border-gray-100"
       >
         <Text className="text-gray-800 font-medium">Send Personalized Notifications</Text>
+        <Ionicons name="chevron-forward" size={20} color="#9CA3AF" />
+      </TouchableOpacity>
+      
+      <TouchableOpacity
+        onPress={onSubmangerPress }
+        className="flex-row justify-between items-center px-4 py-4 border-b border-gray-100"
+      >
+        <Text className="text-gray-800 font-medium">Submanagers</Text>
         <Ionicons name="chevron-forward" size={20} color="#9CA3AF" />
       </TouchableOpacity>
     </View>
